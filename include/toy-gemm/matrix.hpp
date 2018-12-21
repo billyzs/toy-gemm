76cab36f39ac3db17d90f39a75eae42fb755af2d
Billy Zhou <billyzs.728@gmail.com>
2018-12-21 01:22:19 -0500

    matrix: add special functions
    - zeros() and ones() uniformly initializes a matrix
    - identity() returns a square matrix with diagnoal entries = 1
    - all implemented as static methods

--- include/toy-gemm/matrix.hpp
+++ include/toy-gemm/matrix.hpp
@@ -12,25 +12,10 @@
 
 namespace toy_gemm
 {
-namespace internal
-{
-struct RowVecTag final {
-};
-struct ColVecTag final {
-};
-
-template <typename RetType, typename ElemType, size_t... idx>
-constexpr RetType list_item(std::initializer_list<ElemType> &&l, std::index_sequence<idx...>)
-{
-    return {(*std::next(std::begin(std::move(l)), idx))...};
-}
-}  // namespace internal
-
-///< @brief Vec is a thin wrapper around std::array representing a 1D vector
 template <typename T, size_t C>
-using Vec = std::array<T, C>;
+using Vec = std::array<T, C>;  ///< choosing std::array to represent a 1D vector
 
-template <size_t R, size_t C, typename T = int>
+template <size_t R, size_t C = R, typename T = int>
 class Mat
 {
    public:
@@ -41,10 +26,10 @@ class Mat
     using TRef = typename RowType::reference;
     using TCRef = typename RowType::const_reference;
     using StorageType = std::array<RowType, R>;
-    using RowIterator = typename RowType::iterator;
-    using ConstRowIterator = typename RowType::const_iterator;
 
     constexpr static size_t ELEM_COUNT = R * C;
+    constexpr static size_t ROW_COUNT = R;
+    constexpr static size_t COL_COUNT = C;
 
     ~Mat<R, C, T>() = default;
 
@@ -82,19 +67,22 @@ class Mat
 
     /**
      * @brief constructor using exactly R initializer_lists
-     * it's nice to be able to initialize a Mat like
-     * @c Mat<3,2> M{{1,2,3},{4,5,6}}
-     * because some compile time dimension checks can be performed
+     * It's nice to be able to initialize a Mat like this because this form leaves little room for ambiguity; consider
+     * @c Mat<3,2> M{1,2,3,4,5,6};
+     * @c Mat<2,3> M{1,2,3,4,5,6};
+     * they look very similar, it's not immediately obvious if someone swapped the dimension by mistake; compared with
+     * @c Mat<3,2> M{{1,2,3},{4,5,6}};
+     * in which the mistake is much more obvious.
      * @note we don't have compile time non-empty initializer_list yet, otherwise we could make the whole thing
      * constexpr
      */
-    template <typename... E>  //, std::enable_if_t<R == sizeof...(E), int> = 0>
+    template <typename... E>
     explicit Mat<R, C, T>(std::initializer_list<E> &&... l)
     {
-        static_assert(R == sizeof...(l));
-        const bool every_list_must_have_C_elements = ((C == l.size()) && ...);
+        static_assert(ROW_COUNT == sizeof...(l));
+        const bool every_list_must_have_C_elements = ((COL_COUNT == l.size()) && ...);  // C++17 fold expression
         assert(every_list_must_have_C_elements);
-        make(std::move(l)...);
+        row_wise_init(std::move(l)...);
     }
 
     // access (might throw)
@@ -128,7 +116,7 @@ class Mat
 
     // access (noexcept); prefer these, which gives compile time error if indices are out of range
     template <size_t row>
-    [[nodiscard]] RowType &get() noexcept
+    [[nodiscard]] constexpr RowType &get() noexcept
     {
         return std::get<row>(elems);
     }
@@ -140,42 +128,49 @@ class Mat
     }
 
     template <size_t row, size_t col>
-    [[nodiscard]] T &get() noexcept
+    [[nodiscard]] constexpr T &get() noexcept
     {
-        return std::get<col>(std::get<row>(elems));
+        return std::get<col>(get<row>());
     }
 
     template <size_t row, size_t col>
     [[nodiscard]] constexpr const T &get() const noexcept
     {
-        return std::get<col>(std::get<row>(elems));
+        return std::get<col>(get<row>());
     }
-
-    [[nodiscard]] StorageType &rows() noexcept { return elems; }
 
     [[nodiscard]] constexpr const StorageType &rows() const noexcept { return elems; }
 
+    /**
+     * @brief return a copy of column at Col
+     * @tparam Col the column to copy
+     * @return copy of a column
+     */
     template <size_t Col>
     [[nodiscard]] constexpr ColType get_col() const noexcept
     {
         return GetCol<Col>::impl(elems, std::make_index_sequence<R>());
     }
 
+    /**
+     * @brief return a tuple of length R containing references to elements at column Col
+     * so far, using tuple of references to represent a "view" of a column has worked well enough; operations such as
+     * iterating over the elements in a column, making a copy from the view, or modifying a column as a whole,
+     * are supported; see how \ref transpose and \ref operator* uses this
+     * @tparam Col the column to view
+     * @return a tuple containing R lvalue references to column Col
+     */
     template <size_t Col>
-    [[nodiscard]] ColType get_col() noexcept
+    [[nodiscard]] constexpr auto col_view() noexcept
     {
-        return GetCol<Col>::impl(elems, std::make_index_sequence<R>());
-    }
-
-    template <size_t Col>
-    [[nodiscard]] constexpr auto get_col_view() noexcept
-    {
-        // return a tuple of length R containing references to elements at column Col
         return GetColView<Col>::impl(elems, std::make_index_sequence<R>());
     }
 
+    /**
+     * @brief the const overload of \ref col_view
+     */
     template <size_t Col>
-    [[nodiscard]] constexpr auto get_col_view() const noexcept
+    [[nodiscard]] constexpr auto col_view() const noexcept
     {
         // return a tuple of length R containing references to elements at column Col
         return GetColView<Col>::impl(elems, std::make_index_sequence<R>());
@@ -184,9 +179,8 @@ class Mat
     // operators
     [[nodiscard]] constexpr bool operator==(const ThisType &other) const noexcept
     {
-        // could do return elems == other.elems but libstdc++ did not implement == for arrays as constexpr...
-        // return equal(other.elems, std::make_index_sequence<R>());
-        return elems == other.elems;
+        // could do return elems == other.elems but libstdc++ did not implement == for arrays as constexpr :(
+        return equal(elems, other.elems, std::make_index_sequence<ROW_COUNT>());
     }
 
     [[nodiscard]] constexpr bool operator!=(const ThisType &other) const noexcept { return !this->operator==(other); }
@@ -194,87 +188,138 @@ class Mat
     template <size_t OtherC, typename E>
     [[nodiscard]] constexpr auto operator*(const Mat<C, OtherC, E> &other) const noexcept
     {
-        using RetElement = decltype(std::declval<E>() *
-                                    std::declval<T>());  // the type of the return element should be the type produced
-                                                         // by multiplying an instance of T with an instance of E
+        // the type of the return element should be the type produced by multiplying an instance of T with an instance
+        // of E, taking promotion into account
+        using RetElement = decltype(std::declval<E>() * std::declval<T>());
         using RetType = Mat<R, OtherC, RetElement>;
-        constexpr auto make_ret_mat = [](auto... e) { return RetType{e...}; };
+
+        // using the element-wise initialization overload
+        constexpr auto make_ret_mat = [](auto... e) {  // C++17 variadic lambda
+            static_assert(ROW_COUNT * OtherC == sizeof...(e), "must be given ROW_COUNT * OtherC elements");
+            return RetType{e...};
+        };
+
+        // C++17 apply
         return std::apply(make_ret_mat,
                           MulImpl<RetElement, OtherC>::build_mat(elems, other, std::make_index_sequence<R>()));
     }
 
+    /**
+     * @return return the transpose of this matrix by value
+     */
     [[nodiscard]] constexpr Mat<C, R, T> transpose() const noexcept
     {
         return transpose_impl(std::make_index_sequence<C>());
     }
+    // TODO maybe it's also possible to return a view of the transpose of this matrix?
+
+    // special functions; for demo
+    static constexpr ThisType zeros() noexcept { return ThisType{0}; }
+
+    static constexpr Mat<R, R, T> identity() noexcept
+    {
+        static_assert(ROW_COUNT == COL_COUNT, "only defined for square matrices");
+        Mat<R, R, T> ret{0};
+        ret.fill_diagonal(T{1}, std::make_index_sequence<R>());
+        return ret;
+    }
 
    private:
     template <size_t OR, size_t OC, typename OT>
-    friend class Mat;
+    friend class Mat;  ///< for ease of interoperability with another instance of this class
 
     StorageType elems{0};  ///< row-major 2D array, defaults to zero-initialized
-    // explicit Mat<R,C,T>(StorageType&& e) : elems(std::move(e)){}
 
     /**
-     * @brief helper struct to get the column
-     * Can't have get_impl take in both a size_t tparam and a size_t... param pack, so passing Col via the tparams of
-     * this struct
-     * @tparam Col the column to get
+     * @brief fill the (main) diagonal with a given value
+     * @tparam Rows usually ROW_COUNT
+     * @param val the value to fill with
      */
-    template <size_t Col>
-    struct GetCol final {
-        GetCol() = delete;
-        template <size_t... idx>
-        [[nodiscard]] static constexpr ColType impl(const StorageType &storage, std::index_sequence<idx...>) noexcept
-        {
-            return {std::get<Col>(std::get<idx>(storage))...};
-        }
-        template <size_t... idx>
-        [[nodiscard]] static ColType impl(StorageType &storage, std::index_sequence<idx...>) noexcept
-        {
-            return {std::get<Col>(std::get<idx>(storage))...};  // call get<Col> on every "row" in storage
-        }
-    };
-
-    // TODO try SFINAE
-    constexpr RowType make_row(std::initializer_list<T> &&l)
+    template <size_t... Rows>
+    constexpr void fill_diagonal(T val, std::index_sequence<Rows...>) noexcept
     {
-        return internal::list_item<RowType, T>(std::move(l), std::make_index_sequence<C>());
+        ((std::get<Rows>(std::get<Rows>(elems)) = val), ...);  // C++17 fold expression
     }
 
-    template <size_t... idx>
-    constexpr auto rows(std::index_sequence<idx...>)
+    /**
+     * @brief unpacks an initializer list to make a row
+     * @tparam Cols usually COL_COUNT
+     * @param l an initializer list
+     * @return a row with elements from l, in the order they appear
+     */
+    template <size_t... Cols>
+    constexpr RowType make_row(std::initializer_list<T> &&l, std::index_sequence<Cols...>)
     {
-        return std::forward_as_tuple(elems[idx]...);
+        // TODO maybe assert on size of list
+        return {(*std::next(std::begin(std::move(l)), Cols))...};
+    }
+
+    /**
+     * @brief return a tuple containing references to each row
+     * @tparam Rows usually ROW_COUNT
+     * @return tuple containing lvalue references to rows in elems
+     */
+    template <size_t... Rows>
+    constexpr auto rows(std::index_sequence<Rows...>)
+    {
+        return std::forward_as_tuple(std::get<Rows>(elems)...);
     }
 
     template <typename... L>
-    constexpr void make(std::initializer_list<L> &&... l)
+    constexpr void row_wise_init(std::initializer_list<L> &&... l)
     {
         constexpr auto seq = std::index_sequence_for<L...>();
-        rows(seq) = std::forward_as_tuple(make_row(std::move(l))...);
+        rows(seq) = std::forward_as_tuple(make_row(std::move(l), std::make_index_sequence<C>())...);
     }
 
     template <size_t... idx>
     constexpr Mat<C, R, T> transpose_impl(std::index_sequence<idx...>) const noexcept
     {
-        constexpr auto make_transpose_mat = [](auto... e) { return Mat<C, R, T>{e...}; };
-        return std::apply(make_transpose_mat, std::tuple_cat(get_col_view<idx>()...));
+        constexpr auto make_transpose_mat = [](auto... e) { return Mat<C, R, T>{e...}; };  // C++17 variadic lambdas
+        return std::apply(make_transpose_mat, std::tuple_cat(col_view<idx>()...));
     }
+
+    /**
+     * @brief helper struct to get a copy of a column
+     * Can't have get_impl take in both a size_t template param and a size_t... param pack, so passing Col via the
+     * template params of this struct
+     * @tparam Col the column to get
+     */
+    template <size_t Col>
+    struct GetCol final {
+        GetCol() = delete;  ///< don't bother generating special functions
+
+        template <typename SType, size_t... idx>
+        [[nodiscard]] static constexpr ColType impl(SType &&storage, std::index_sequence<idx...>) noexcept
+        {
+            static_assert(COL_COUNT == sizeof...(idx), "should be getting exactly COL_COUNT elements");
+            return {std::get<Col>(std::get<idx>(std::forward<SType>(storage)))...};
+        }
+    };
 
     template <size_t Col>
     struct GetColView final {
-        GetColView() = delete;
-        template <typename SType, size_t... Row>
-        static constexpr auto impl(SType &&storage, std::index_sequence<Row...>) noexcept
+        GetColView() = delete;  ///< don't bother generating special functions
+
+        /**
+         * @brief return a tuple of references to elements in a column
+         * @tparam SType usually StorageType
+         * @tparam Rows  usually ROW_COUNT
+         * @param storage usually elems
+         * @return tuple of references to elements in a column; constness of the references depends on constness of
+         * storage
+         */
+        template <typename SType, size_t... Rows>
+        static constexpr auto impl(SType &&storage, std::index_sequence<Rows...>) noexcept
         {
-            return std::forward_as_tuple(std::get<Col>(std::get<Row>(std::forward<SType>(storage)))...);
+            return std::forward_as_tuple(std::get<Col>(std::get<Rows>(std::forward<SType>(storage)))...);
         }
     };
 
     template <typename ElemType, size_t OCol>
     struct MulImpl final {
-        MulImpl() = delete;
+        MulImpl() = delete;  ///< don't bother generating special functions
+
         /**
          * @brief constexpr dot product of two arrays of same length
          * @tparam OtherCol type of a col from the rhs matrix
@@ -287,36 +332,84 @@ class Mat
         [[nodiscard]] constexpr static ElemType inner_product(const RowType &this_row, const OtherCol &other_col,
                                                               std::index_sequence<Cols...>) noexcept
         {
-            // this_row should be of the same length as other_col (C)
-            constexpr auto sum = [](auto... e) { return (e + ...); };
-            return std::apply(sum, std::forward_as_tuple(std::get<Cols>(this_row) * std::get<Cols>(other_col)...));
+            constexpr auto accumulate = [](auto... e) -> ElemType {  // C++17 variadic lambda
+                return (e + ...);  // sum all elements of a param pack; C++17 fold expression
+            };
+            return std::apply(accumulate,
+                              std::forward_as_tuple(std::get<Cols>(this_row) * std::get<Cols>(other_col)...));
         }
 
+        /**
+         * @brief build one row of the output matrix
+         * @tparam Row the row to build
+         * @tparam OtherMat type of the rhs matrix
+         * @tparam OCols number of columns in the output matrix
+         * @param this_storage the underlying 2D array in the lhs matrix
+         * @param other_matt he underlying 2D array in the rhs matrix
+         * @return tuple containing R counts of ElemType
+         */
         template <size_t Row, typename OtherMat, size_t... OCols>
         [[nodiscard]] constexpr static auto build_row(const StorageType &this_storage, const OtherMat &other_mat,
                                                       std::index_sequence<OCols...>) noexcept
         {
-            return std::make_tuple(inner_product(std::get<Row>(this_storage), other_mat.template get_col_view<OCols>(),
+            return std::make_tuple(inner_product(std::get<Row>(this_storage),
+                                                 other_mat.template col_view<OCols>(),  // C++ template disambiguator
                                                  std::make_index_sequence<C>())...);
         }
+
+        /**
+         * @brief build a tuple of ElemType, containing R * OCol elements that shall be used to element-wise construct a
+         * Mat<Rows, OCol, ElemType>
+         * @tparam OtherMat type of the rhs matrix
+         * @tparam Rows number of rows in the output matrix; should be R
+         * @param this_storage the underlying 2D array in the lhs matrix
+         * @param other_mat underlying 2D array in the rhs matrix
+         * @return tuple containing R * OCol counts of ElemType
+         */
         template <typename OtherMat, size_t... Rows>
-        [[nodiscard]] constexpr static auto build_mat(const StorageType &this_storage, const OtherMat &other_storage,
+        [[nodiscard]] constexpr static auto build_mat(const StorageType &this_storage, const OtherMat &other_mat,
                                                       std::index_sequence<Rows...>) noexcept
         {
-            // TODO this might be bad because parameter pack expansion is not guaranteed to be ordered; think of a way
-            // to use init list return std::apply(std::tuple_cat, std::forward_as_tuple({build_row<Rows>(this_storage,
-            // other_storage, std::make_index_sequence<OCol>())...}));
-            return std::tuple_cat(build_row<Rows>(this_storage, other_storage, std::make_index_sequence<OCol>())...);
+            static_assert(COL_COUNT == OtherMat::ROW_COUNT,
+                          "the other matrix should have the same number of rows as the columns in this matrix");
+            return std::tuple_cat(build_row<Rows>(this_storage, other_mat, std::make_index_sequence<OCol>())...);
         }
     };
 
-    template <size_t... idx>
-    constexpr bool equal(const StorageType &other, std::index_sequence<idx...>) const noexcept
+    /**
+     * @brief the implementation of \ref row_equal
+     */
+    template <size_t... Cols>
+    static constexpr bool row_equal_impl(const RowType &self, const RowType &other,
+                                         std::index_sequence<Cols...>) noexcept
     {
-        constexpr auto e = [](const RowType &r1, const RowType &r2, auto... col) {
-            return ([&r1, &r2, &col]() { return std::get<col>(r1) == std::get<col>(r2); } && ...);
-        };
-        return (e(std::get<idx>(elems), std::get<idx>(other)) && ...);
+        return ((std::get<Cols>(self) == std::get<Cols>(other)) && ...);  // C++17 fold expression
+    }
+
+    /**
+     * @brief check if two rows are equal;
+     * @tparam Row the row to compare
+     * @param self storage of the lhs of operator==
+     * @param other storage of the rhs of operator==
+     * @return true if every element in row Row of self is equal to the element in the corresponding position in other
+     */
+    template <size_t Row>
+    static constexpr bool row_equal(const StorageType &self, const StorageType &other) noexcept
+    {
+        return row_equal_impl(std::get<Row>(self), std::get<Row>(other), std::make_index_sequence<C>());
+    }
+
+    /**
+     * @brief check if all the rows in self are equal to all the rows in other
+     * @tparam Rows usually ROW_COUNT
+     * @param self storage of the lhs of operator==
+     * @param other storage of the rhs of operator==
+     * @return true if all the rows in self are equal to all the rows in other; false otherwise
+     */
+    template <size_t... Rows>
+    static constexpr bool equal(const StorageType &self, const StorageType &other, std::index_sequence<Rows...>)
+    {
+        return ((row_equal<Rows>(self, other)) && ...);  // C++17 fold expression
     }
 };
 
--- test/test-ctor.cpp
+++ test/test-ctor.cpp
@@ -5,36 +5,40 @@ using namespace toy_gemm;
 using M22 = Mat<2, 2>;
 using M23 = Mat<2, 3>;
 using M32 = Mat<3, 2>;
-TEST(toy_gemm_ctor, ctor)
+using M33 = Mat<3>;
+
+TEST(toy_gemm, ctor)
 {
-    constexpr Mat<3, 3, int> x;
+    constexpr M33 x;
     static_assert(!std::is_constructible_v<decltype(x), int, int, int, int, int, int>,
                   "only constructible from either 1 or 9 arguments");
-    constexpr decltype(x) zeros{0};
+    constexpr decltype(x) zeros(0);
     constexpr decltype(x) y(0, 0, 0, 0, 0, 0, 0, 0, 0);
-    ASSERT_EQ(x, zeros);
-    ASSERT_EQ(y, zeros);
-    Mat<3, 2, int> z{{1, 2}, {3, 4}, {5, 6}};
-    constexpr Mat<3, 2, int> z2{1, 2, 3, 4, 5, 6};
-    ASSERT_EQ(z, z2);
+    static_assert(x == zeros);
+    static_assert(y == zeros);
+    constexpr M32 z{1, 2, 3, 4, 5, 6};
+    M32 z_list_ctor{{1, 2}, {3, 4}, {5, 6}};  // sadly unable to constexpr this
+    M32 z_copy_ctor(z);
+    ASSERT_EQ(z, z_list_ctor);
+    ASSERT_EQ(z, z_copy_ctor);
 }
 
 TEST(toy_gemm, accessor)
 {
-    constexpr Mat<3, 3> I3{1, 0, 0, 0, 1, 0, 0, 0, 1};
+    constexpr M33 I3{1, 0, 0, 0, 1, 0, 0, 0, 1};
     constexpr auto first_row = Vec<int, 3>{1, 0, 0};
     constexpr auto second_row = Vec<int, 3>{0, 1, 0};
     ASSERT_EQ(I3.get<0>(), first_row);
     ASSERT_EQ(I3[1], second_row);
-    static_assert(I3.get<2, 0>() == 0, "test templated get<>()");
-    ASSERT_EQ(I3[2][1], 0);
-    ASSERT_EQ(I3.at(2, 2), 1);
+    static_assert(I3.get<2, 2>() == 1, "test template get<>()");
+    ASSERT_EQ(I3[1][1], 1);
+    ASSERT_EQ(I3.at(0, 0), 1);
 }
 
 TEST(toy_gemm, rows)
 {
     const M23 m23({1, 2, 3}, {4, 5, 6});
-    const Mat<2, 3, int> m23_dup = m23;
+    const M23 m23_dup = m23;
     size_t r = 0;
     for (const auto& row : m23.rows()) {  // shall be compatible with range for
         ASSERT_EQ(row.size(), 3);
@@ -43,9 +47,33 @@ TEST(toy_gemm, rows)
     ASSERT_EQ(r, 2);
 }
 
+TEST(toy_gemm_access, col)
+{
+    constexpr M22 x{1, 2, 3, 4};
+    constexpr auto xcol1 = x.get_col<0>();
+    constexpr M22::ColType col1{1, 3};
+    constexpr auto xcol2 = x.get_col<1>();
+    constexpr M22::ColType col2{2, 4};
+    ASSERT_EQ(col1, xcol1);
+    ASSERT_EQ(col2, xcol2);
+
+    const auto xcolv2 = x.col_view<1>();
+    static_assert(std::is_same_v<std::tuple<const int&, const int&>, std::remove_cv_t<decltype(xcolv2)>>);
+    const auto [c1, c2] = xcolv2;  // C++17 structured binding
+    ASSERT_EQ(c1, 2);
+    ASSERT_EQ(c2, 4);
+
+    auto y = x;
+    auto ycol1 = y.get_col<0>();
+    ASSERT_EQ(col1, ycol1);
+
+    y.col_view<1>() = std::make_tuple(0, 0);
+    constexpr M22 yy{1, 0, 3, 0};
+    ASSERT_EQ(y, yy);
+}
+
 TEST(toy_gemm_ops, comparison)
 {
-    using M33 = Mat<3, 3, int>;
     constexpr M33 x;
     M33 y{0};
     ASSERT_EQ(x, y);
@@ -57,7 +85,7 @@ TEST(toy_gemm_ops, multiplication)
 {
     constexpr M22 x{1, 2, 3, 4};
     constexpr M22 y{1, 0, 0, 1};
-    M22 z = x * y;
+    constexpr M22 z = x * y;
     auto zz = z * y;
     ASSERT_EQ(z, x);
     ASSERT_EQ(y * y, y);
@@ -71,32 +99,19 @@ TEST(toy_gemm_ops, transpose)
     constexpr M32 m23_t{1, 4, 2, 5, 3, 6};
     constexpr M32 m32 = m23.transpose();
     static_assert(std::is_same_v<M32, std::remove_cv_t<decltype(m32)>>, "type must match");
-    // static_assert(m32 == m23_t, "value must match");
-    ASSERT_EQ(m32, m23_t);
+    static_assert(m32 == m23_t, "value must match");
 }
 
-TEST(toy_gemm_access, get_col)
+TEST(toy_gemm, special_functions)
 {
-    constexpr M22 x{1, 2, 3, 4};
-    constexpr auto xcol1 = x.get_col<0>();
-    constexpr M22::ColType col1{1, 3};
-    constexpr auto xcol2 = x.get_col<1>();
-    constexpr M22::ColType col2{2, 4};
-    ASSERT_EQ(col1, xcol1);
-    ASSERT_EQ(col2, xcol2);
+    constexpr M33 Z3{0, 0, 0, 0, 0, 0, 0, 0, 0};
+    constexpr M33 z3 = M33::zeros();
+    static_assert(z3 == Z3);
 
-    // looks like the ctor of tuple is not constexpr, otherwise this could be constexpr too
-    // this does on constexpr Mat which is pretty cool, but I admit it's not very useful yet :P
-    std::tuple<const int&, const int&> xcolv2 = x.get_col_view<1>();
-    auto [c1, c2] = xcolv2;  // structured binding
-    ASSERT_EQ(c1, 2);
-    ASSERT_EQ(c2, 4);
+    constexpr M33 I3{1, 0, 0, 0, 1, 0, 0, 0, 1};
+    constexpr M33 i3 = Mat<3>::identity();
+    static_assert(i3 == I3);
 
-    auto y = x;
-    auto ycol1 = y.get_col<0>();
-    ASSERT_EQ(col1, ycol1);
-
-    y.get_col_view<1>() = std::make_tuple(0, 0);
-    constexpr M22 yy{1, 0, 3, 0};
-    ASSERT_EQ(y, yy);
+    constexpr auto I16 = Mat<16>::identity();
+    static_assert(I16.transpose() == I16); // this is can be a bit hard for the compiler
 }
